e16bcc17e0e06135be3aa2e17efdf6077bc7ad89
Dasun Pubudumal <pubudumald@gmail.com>
2024-10-11 15:00:18 +0100

    Refactor imports in lib.rs and main.rs, and add base64 crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate dotenv_codegen;
 
+use std::collections::HashMap;
+use base64::prelude::*;
+
 use rabbitmq_messages_management::{prepare_url, send_get};
 use serde::{Deserialize, Serialize};
 
@@ -8,18 +11,33 @@ mod rabbitmq;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Ip {
-    origin: String
+    origin: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-
     let result: Ip = send_get(
         &prepare_url("http://httpbin.org", "ip").unwrap(),
-        None
-    ).await.unwrap();
+        None,
+    )
+    .await
+    .unwrap();
 
-    println!("{:?}", result);
+    let management_root = dotenv!("RABBITMQ_MANAGEMENT_ROOT");
+    let management_username = dotenv!("RABBITMQ_MANAGEMENT_USERNAME");
+    let management_pw = dotenv!("RABBITMQ_MANAGEMENT_PASSWORD");
+
+    let map = &HashMap::from([(
+        "Authorization".to_string(),
+        format!(
+            "Basic {}",
+            BASE64_STANDARD.encode(format!("{}:{}", management_username, management_pw))
+        ),
+    )]);
+
+    let rabbitmq_result: String = send_get(management_root, Some(map)).await.unwrap();
+
+    println!("{:?}", rabbitmq_result);
 
     Ok(())
 }
