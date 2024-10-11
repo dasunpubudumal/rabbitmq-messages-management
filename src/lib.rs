b16ec7167a15d36cdb514a57f2065757a3c050d5
Dasun Pubudumal <pubudumald@gmail.com>
2024-10-11 14:16:44 +0100

    Add RabbitMQ management credentials to .env.example and update .gitignore
    - Added RABBITMQ_MANAGEMENT_USERNAME and RABBITMQ_MANAGEMENT_PASSWORD to .env.example
    - Updated .gitignore to include .env file

---
+++ src/lib.rs
@@ -0,0 +1,81 @@
+extern crate hyper;
+
+use hyper::Uri;
+use serde::{de::Error, Deserialize};
+use tokio::net::TcpStream;
+use hyper_util::rt::TokioIo;
+use hyper::Request;
+use hyper::client::conn::http1::Builder;
+use http_body_util::{BodyExt, Empty};
+use hyper::body::Bytes;
+
+/// Sends an HTTP GET request to
+#[derive(PartialEq)]
+pub enum Authority {
+  HTTP,
+  HTTPS
+}
+
+/// Sends HTTP GET request to authority://host:port/path
+/// Returns a response.
+pub async fn send_get<T>(uri: &str) -> Result<T, ()>
+where
+    T: for<'de> Deserialize<'de>,
+{
+
+  let url = uri.parse::<hyper::Uri>().unwrap();
+
+    // Get the host and the port
+  let host = url.host().expect("uri has no host");
+  let port = url.port_u16().unwrap_or(80);
+
+  let address = format!("{}:{}", host, port);
+
+  // Establish a TCP connection with the remote host.
+  let stream = TcpStream::connect(address).await.unwrap();
+
+  // Required to read from the stream
+  let io = TokioIo::new(stream);
+
+  let authority = url.authority().unwrap().clone();
+
+  let path = url.path();
+  let req = Request::builder()
+      .uri(path)
+      .header(hyper::header::HOST, authority.as_str())
+      .body(Empty::<Bytes>::new())
+      .unwrap();
+
+  // Create hyper HTTP client
+  let (mut sender, conn) = Builder::new()
+      .handshake::<_, Empty<Bytes>>(io)
+      .await
+      .unwrap();
+
+  tokio::task::spawn(async move {
+      if let Err(err) = conn.await {
+          println!("Connection failed: {:?}", err);
+      }
+  });
+
+  // Obtain the response stream (frame)
+  let mut res = sender.send_request(req).await.unwrap();
+
+  println!("Response status: {}", res.status());
+
+  let mut response_body = Vec::new();
+
+  while let Some(next) = res.frame().await {
+      let frame = next.unwrap();
+      if let Some(chunk) = frame.data_ref() {
+          response_body.extend_from_slice(chunk);
+      }
+  }
+
+  // Move the deserialization step outside the loop
+  let response_data: T = serde_json::from_slice(&response_body).unwrap();
+
+  Ok(response_data)
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,22 @@
-use http_body_util::{BodyExt, Empty};
-use hyper::client::conn::http1::Builder;
-use hyper::Request;
-use hyper::body::Bytes;
-use hyper_util::rt::TokioIo;
-use serde::Deserialize;
-use tokio::net::TcpStream;
+#[macro_use]
+extern crate dotenv_codegen;
 
-#[derive(Debug, Deserialize)]
-struct ResponseData {
-    pub origin: String,
+use rabbitmq_messages_management::{send_get, Authority};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Ip {
+    origin: String
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
-    // Parse or panic
-    let url = "http://httpbin.org/ip".parse::<hyper::Uri>()?;
+    let result: Ip = send_get(
+        "http://httpbin.org/ip"
+    ).await.unwrap();
 
-    let host = url.host().expect("URI has no host");
-    let port = url.port_u16().unwrap_or(80);    // Default web port
-    let address = format!("{}:{}", host, port);
-
-    // Establish a TCP connection with the remote host.
-    let stream = TcpStream::connect(address).await?;
-
-    // Required to read from the stream
-    let io = TokioIo::new(stream);
-
-    let authority = url.authority().unwrap().clone();
-    let path = url.path();
-    let req = Request::builder()
-                            .uri(path)
-                            .header(hyper::header::HOST, authority.as_str())
-                            .body(Empty::<Bytes>::new())?;
-
-    // Create hyper HTTP client
-    let (mut sender, conn) 
-        = Builder::new()
-            .handshake::<TokioIo<tokio::net::TcpStream>, http_body_util::Empty<Bytes>>(io)
-            .await?;
-
-    tokio::task::spawn(async move {
-        if let Err(err) = conn.await {
-            println!("Connection failed: {:?}", err);
-        }
-    });
-
-    // Obtain the response stream (frame)
-    let mut res = sender.send_request(req).await?;
-
-    println!("Response status: {}", res.status());
-    println!("Headers: {:#?}", res.headers());
-
-    let mut response_body = Vec::new();
-
-    while let Some(next) = res.frame().await {
-        let frame = next?;
-        if let Some(chunk) = frame.data_ref() {
-            response_body.extend_from_slice(chunk);
-        }
-    }
-
-    let response_data: ResponseData = serde_json::from_slice(&response_body)?;
-
-    println!("Origin: {}", response_data.origin);
+    println!("{:?}", result);
 
     Ok(())
 }
