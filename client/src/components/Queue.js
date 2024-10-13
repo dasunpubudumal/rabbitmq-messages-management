65949a8debd0d5aa36353e3632824c743b3dc762
Dasun Pubudumal <pubudumald@gmail.com>
2024-10-13 16:00:40 +0100

    Refactor Queue component to remove unused useEffect import

--- client/src/components/Queue.js
+++ client/src/components/Queue.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
