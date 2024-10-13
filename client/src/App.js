76dd7e4bff5a9331db7e06204ad97ecba91e75d1
Dasun Pubudumal <pubudumald@gmail.com>
2024-10-13 14:31:06 +0100

    Add dependencies for @emotion/react, @emotion/styled, and @mui/material in client/package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import JsonView from "@uiw/react-json-view";
 import { useEffect, useState } from "react";
 
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
 export default function App() {
   const [vhosts, setVhosts] = useState([]);
+  const [selectedVhost, setSelectedVhost] = useState("");
 
   useEffect(() => {
     async function responses() {
@@ -12,8 +19,28 @@ export default function App() {
     responses();
   }, []);
 
+  const handleChange = (event) => {
+    setSelectedVhost(event.target.value);
+  };
+
   return (
     <>
+      <FormControl fullWidth>
+        <InputLabel id="vhost-select">Select Vhost</InputLabel>
+        <Select
+          labelId="vhost-select"
+          id="vhost-select"
+          value={selectedVhost}
+          label="Age"
+          onChange={handleChange}
+        >
+          {vhosts.map((vhost, index) => (
+            <MenuItem value={vhost.name} key={index}>
+              {vhost.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <JsonView value={vhosts} />
     </>
   );
