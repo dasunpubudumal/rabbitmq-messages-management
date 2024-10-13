037d0d0baee7838448cd0bf199a00d5887f51529
Dasun Pubudumal <pubudumald@gmail.com>
2024-10-13 14:37:55 +0100

    Add @toolpad/core dependency and update UI components in client package.json and App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import JsonView from "@uiw/react-json-view";
 import { useEffect, useState } from "react";
-
-import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import { PageContainer } from '@toolpad/core/PageContainer';
+import { Container } from "@mui/material";
 
 export default function App() {
   const [vhosts, setVhosts] = useState([]);
@@ -25,23 +25,27 @@ export default function App() {
 
   return (
     <>
-      <FormControl fullWidth>
-        <InputLabel id="vhost-select">Select Vhost</InputLabel>
-        <Select
-          labelId="vhost-select"
-          id="vhost-select"
-          value={selectedVhost}
-          label="Age"
-          onChange={handleChange}
-        >
-          {vhosts.map((vhost, index) => (
-            <MenuItem value={vhost.name} key={index}>
-              {vhost.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <JsonView value={vhosts} />
+      <PageContainer>
+        <FormControl fullWidth>
+          <InputLabel id="vhost-select">Select Vhost</InputLabel>
+          <Select
+            labelId="vhost-select"
+            id="vhost-select"
+            value={selectedVhost}
+            label="Age"
+            onChange={handleChange}
+          >
+            {vhosts.map((vhost, index) => (
+              <MenuItem value={vhost.name} key={index}>
+                {vhost.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Container>
+          <JsonView value={vhosts} />
+        </Container>
+      </PageContainer>
     </>
   );
 }
